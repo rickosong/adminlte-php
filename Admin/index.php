243a72434a3d46ae3e93ac5e508eff70b3c522b1
Mohammad Ricko Aprilianto <ricko.aprilianto00@gmail.com>
2024-06-22 11:32:23 +0800

    add sesssion for logout

--- Admin/index.php
+++ Admin/index.php
@@ -1,13 +1,16 @@
 <?php
 session_start();
 include "conf/conn.php";
+if(isset($_SESSION['id_admin'])==0){
+  echo '<script>alert("Anda Harus Login Terlebih Dahulu !!!");
+  window.location.href="pages/login.php"</script>';
+  }else{
 ?>
 
-
 <?php include 'template_header.php' ?>
-
-
 <?php include 'template_footer.php' ?>
+
+<?php } ?>
 
 <!-- REQUIRED SCRIPTS -->
 <?php include 'partials/js-file.php' ?>
